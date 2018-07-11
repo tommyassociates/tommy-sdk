1c04f81dbb9a309125a1664c33acf294748a9b75
Vladimir Kharlampidi <nolimits4web@gmail.com>
2018-07-11 16:12:09 +0300

    Package list page

--- addons/nurse_booking/1.0.0/views/javascripts/index.js
+++ addons/nurse_booking/1.0.0/views/javascripts/index.js
@@ -1,4 +1,5 @@
 import IndexController from './controllers/index'
+import PackageListController from './controllers/package-list';
 import currencyMap from './currency-map';
 
 const { f7, t7 } = window.tommy.app;
@@ -7,7 +8,10 @@ const { f7, t7 } = window.tommy.app;
 f7.onPageInit('nurse_booking__index', IndexController.init)
 f7.onPageBeforeRemove('nurse_booking__index', IndexController.uninit)
 
-t7.registerHelper('wallet__currencySymbol', code => {
+f7.onPageInit('nurse_booking__package-list', PackageListController.init)
+f7.onPageBeforeRemove('nurse_booking__package-list', PackageListController.uninit)
+
+t7.registerHelper('nurse_booking__currencySymbol', code => {
   return currencyMap(code);
 });
 
