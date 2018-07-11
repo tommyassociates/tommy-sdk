0ccf0a13d15bcf596a7e7fa91d4718ec5a54715f
Vladimir Kharlampidi <nolimits4web@gmail.com>
2018-07-11 17:19:47 +0300

    Package details page layout

--- addons/nurse_booking/1.0.0/views/javascripts/index.js
+++ addons/nurse_booking/1.0.0/views/javascripts/index.js
@@ -1,5 +1,6 @@
 import IndexController from './controllers/index'
 import PackageListController from './controllers/package-list';
+import PackageDetailsController from './controllers/package-details';
 import currencyMap from './currency-map';
 
 const { f7, t7 } = window.tommy.app;
@@ -10,6 +11,9 @@ f7.onPageBeforeRemove('nurse_booking__index', IndexController.uninit)
 
 f7.onPageInit('nurse_booking__package-list', PackageListController.init)
 f7.onPageBeforeRemove('nurse_booking__package-list', PackageListController.uninit)
+
+f7.onPageInit('nurse_booking__package-details', PackageDetailsController.init)
+f7.onPageBeforeRemove('nurse_booking__package-details', PackageDetailsController.uninit)
 
 t7.registerHelper('nurse_booking__currencySymbol', code => {
   return currencyMap(code);
