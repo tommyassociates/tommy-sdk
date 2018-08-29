d8668d4a048301a9fe483e2e28ceb897e3ab1700
Vladimir Kharlampidi <nolimits4web@gmail.com>
2018-08-29 14:44:37 +0300

    Accounts addon initial setup

---
+++ addons/accounts/1.0.0/views/javascripts/controllers/list-add.js
@@ -0,0 +1,37 @@
+import API from '../api'
+
+const ListAddController = {
+  init (page) {
+    const $page = $$(page.container)
+    const $nav = $$(page.navbarInnerContainer)
+
+    $nav.find('a.save').on('click', ev => {
+      const data = window.tommy.app.f7.formToJSON($page.find('form'))
+      ListAddController.saveList(data)
+      ev.preventDefault()
+    })
+  },
+
+  saveList (data) {
+    let list = {}
+    list.name = data.name
+
+    // Default list filters show transactions tagged with current user
+    list.filters = [ API.currentUserTag() ]
+
+    // console.log('create list', list, data)
+    // if (!list.name) {
+    //   alert('List name must be set')
+    //   return
+    // }
+
+    API.saveList(list).then(ListAddController.afterSave)
+  },
+
+  afterSave (res) {
+    console.log('list saved', res)
+    window.tommy.app.f7view.router.back()
+  }
+}
+
+export default ListAddController
