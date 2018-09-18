e67cc7131fa6c54d183608147ed238ce5ffdad92
Vladimir Kharlampidi <nolimits4web@gmail.com>
2018-09-18 17:24:22 +0300

    Remove console.error

--- addons/wallet_accounts/1.0.0/views/javascripts/controllers/transaction-details.js
+++ addons/wallet_accounts/1.0.0/views/javascripts/controllers/transaction-details.js
@@ -4,7 +4,6 @@ const TransactionDetailsController = {
   init (page) {
     const { id } = page.query;
     const transaction = API.cache.transactions[id];
-    console.error({ transaction });
     window.tommy.tplManager.renderInline('wallet_accounts__transactionDetailsTemplate', transaction, page.container);
   },
   uninit () {},
