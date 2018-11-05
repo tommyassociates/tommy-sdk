76c35846fb62d474cf3d387edff4788a6aa794ae
Vladimir Kharlampidi <nolimits4web@gmail.com>
2018-11-05 16:46:28 +0300

    Item/service - management page

--- addons/invoicing/1.0.0/src/addon.js
+++ addons/invoicing/1.0.0/src/addon.js
@@ -2,6 +2,7 @@ import IndexPage from './pages/index.vue';
 // import TaskAddPage from './pages/task-add.vue';
 // import TaskDetailsPage from './pages/task-details.vue';
 import SettingsPage from './pages/settings.vue';
+import ItemServiceManagementPage from './pages/item-service-management.vue';
 import ListManagementPage from './pages/list-management.vue';
 import ListAddPage from './pages/list-add.vue';
 // import ListEditPage from './pages/list-edit.vue';
@@ -30,6 +31,10 @@ const routes = [
   {
     path: '/invoicing/settings/',
     component: SettingsPage,
+  },
+  {
+    path: '/invoicing/item-service-management/',
+    component: ItemServiceManagementPage,
   },
   {
     path: '/invoicing/list-management/',
---
+++ addons/invoicing/1.0.0/src/pages/item-service-management.vue
@@ -0,0 +1,65 @@
+<template>
+  <f7-page name="invoicing__item-service-management" id="invoicing__item-service-management" class="invoicing-page">
+    <f7-navbar>
+      <tommy-nav-back></tommy-nav-back>
+      <f7-nav-title>{{$t('invoicing.item_service_management.title', 'Items / Service')}}</f7-nav-title>
+      <f7-nav-right>
+        <f7-link @click="add" icon-f7="add"></f7-link>
+      </f7-nav-right>
+    </f7-navbar>
+    <div class="item-service-tabs-links">
+      <f7-link @click="activeTab = 'items'" :class="{active: activeTab === 'items'}">{{$t('invoicing.item_service_management.items_tab')}}</f7-link>
+      <f7-link @click="activeTab = 'packages'" :class="{active: activeTab === 'packages'}">{{$t('invoicing.item_service_management.packages_tab')}}</f7-link>
+    </div>
+    <div class="item-service-tab" v-show="activeTab === 'items'">
+      <f7-searchbar search-container=".invoicing-list-items" :backdrop="false" v-if="items && items.length" :disable-button="false" :placeholder="$t('invoicing.item_service_management.search_items')"></f7-searchbar>
+      <f7-list class="list-custom invoicing-list-items" no-hairlines v-if="items && items.length">
+        <f7-list-item
+          v-for="item in items"
+          :key="item.id"
+          :title="item.name"
+          :link="`/invoicing/item-details/${item.id}/`"
+        ></f7-list-item>
+      </f7-list>
+    </div>
+    <div class="item-service-tab" v-show="activeTab === 'packages'">
+      <f7-searchbar search-container=".invoicing-list-packages" :backdrop="false" v-if="packages && packages.length" :disable-button="false" :placeholder="$t('invoicing.item_service_management.search_packages')"></f7-searchbar>
+      <f7-list class="list-custom invoicing-list-packages" no-hairlines v-if="packages && packages.length">
+        <f7-list-item
+          v-for="item in packages"
+          :key="item.id"
+          :title="item.name"
+          :link="`/invoicing/package-details/${item.id}/`"
+        ></f7-list-item>
+      </f7-list>
+    </div>
+  </f7-page>
+</template>
+<script>
+  import API from '../api';
+
+  export default {
+    data() {
+      return {
+        items: null,
+        packages: null,
+        activeTab: 'items',
+      };
+    },
+    mounted() {
+      const self = this;
+      API.loadItems({}, { cache: false }).then((items) => {
+        self.items = items;
+      });
+      API.loadPackages({}, { cache: false }).then((packages) => {
+        self.packages = packages;
+      });
+    },
+    computed: {
+
+    },
+    methods: {
+      add() {},
+    },
+  };
+</script>
