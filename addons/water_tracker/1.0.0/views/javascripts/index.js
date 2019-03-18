56d7b74cc048f5107d9567ada954eff01485d25c
Vladimir Kharlampidi <nolimits4web@gmail.com>
2019-03-18 13:50:24 +0300

    Water Tracker Addon

--- addons/water_tracker/1.0.0/views/javascripts/index.js
+++
@@ -1,8 +0,0 @@
-import IndexController from './controllers/index'
-
-
-//
-// == Router
-
-window.tommy.app.f7.onPageInit('water_tracker__index', IndexController.init)
-window.tommy.app.f7.onPageBack('water_tracker__index', IndexController.uninit)
