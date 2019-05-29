a38968f8ccc115ecb4998fda3f40540e71017d94
Vladimir Kharlampidi <nolimits4web@gmail.com>
2019-05-29 14:52:59 +0300

    Prevent back to order confirm

--- addons/nurse_booking/1.0.0/src/pages/order-success.vue
+++ addons/nurse_booking/1.0.0/src/pages/order-success.vue
@@ -1,5 +1,5 @@
 <template>
-  <f7-page name="nurse_booking__order-success" id="nurse_booking__order-success" no-navbar>
+  <f7-page @page:afterin="onPageAfterIn" name="nurse_booking__order-success" id="nurse_booking__order-success" no-navbar>
     <div class="order-status-title">{{$t('nurse_booking.order_success.title')}}</div>
     <div class="order-status-img">
       <img :src="`${$addonAssetsUrl}order-success.svg`">
@@ -11,6 +11,27 @@
   </f7-page>
 </template>
 <script>
-  export default {};
+  export default {
+    mounted() {
+      const self = this;
+      document.addEventListener('backbutton', self.onBackButton);
+    },
+    beforeDestroy() {
+      const self = this;
+      document.removeEventListener('backbutton', self.onBackButton);
+    },
+    methods: {
+      onBackButton() {
+        const self = this;
+        self.$f7router.back('/nurse_booking/', { force: true });
+      },
+      onPageAfterIn() {
+        const self = this;
+        setTimeout(() => {
+          self.$f7router.clearPreviousHistory();
+        }, 500);
+      },
+    },
+  };
 </script>
 
