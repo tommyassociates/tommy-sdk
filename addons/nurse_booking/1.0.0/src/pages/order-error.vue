c11db81501139ac4b455fc43f2d538ff71eba49d
Vladimir Kharlampidi <nolimits4web@gmail.com>
2019-06-10 15:40:23 +0300

    Fix back pages from error/cancel

--- addons/nurse_booking/1.0.0/src/pages/order-error.vue
+++ addons/nurse_booking/1.0.0/src/pages/order-error.vue
@@ -5,12 +5,33 @@
       <img :src="`${$addonAssetsUrl}order-success.svg`">
     </div>
     <div class="order-status-buttons">
-      <a href="/nurse_booking/order-details/?masterDetailRoot=true">{{$t('nurse_booking.order_error.details_button')}}</a>
+      <a :href="`/nurse_booking/order-details/?backToHome=true&id=${$f7route.query.id}&masterDetailRoot=true`">{{$t('nurse_booking.order_error.details_button')}}</a>
       <a href="/nurse_booking/" class="no-master-detail" data-clear-previous-history="true">{{$t('nurse_booking.order_error.home_button')}}</a>
     </div>
   </f7-page>
 </template>
 <script>
-  export default {};
+  export default {
+    mounted() {
+      const self = this;
+      document.addEventListener('backbutton', self.onBackButton);
+    },
+    beforeDestroy() {
+      const self = this;
+      document.removeEventListener('backbutton', self.onBackButton);
+    },
+    methods: {
+      onBackButton() {
+        const self = this;
+        self.$f7router.back('/nurse_booking/', { force: true });
+      },
+      onPageAfterIn() {
+        const self = this;
+        setTimeout(() => {
+          self.$f7router.clearPreviousHistory();
+        }, 500);
+      },
+    },
+  };
 </script>
 
