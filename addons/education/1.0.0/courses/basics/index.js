c460df29ff68ed5006c311207461c4277f98b8c8
Vladimir Kharlampidi <nolimits4web@gmail.com>
2019-06-25 17:09:05 +0300

    Education: Video and Quiz questions

--- addons/education/1.0.0/courses/basics/index.js
+++ addons/education/1.0.0/courses/basics/index.js
@@ -1,15 +1,73 @@
 export default {
   id: 'basics',
   icon: 'basics.svg',
-  name: '托me’s Basic',
-  description: `Before you can receive bookings you must first complete the 托me Basics Certificate.
-  This certificate includes training on tasks
-  you will perform during bookings and demonstrates the standard that needs to be maintained.`,
-  price: 'free',
+  indexName: "托me's Basic",
+  indexDescription: 'Before you can receive bookings you must first complete the 托me Basics Certificate. This certificate includes training on tasks you will perform during bookings and demonstrates the standard that needs to be maintained.',
+  pageName: "托me's Basic",
+  pageDescription: 'Before you can receive bookings you must first complete the 托me Basics Certificate. This certificate includes training on tasks you will perform during bookings and demonstrates the standard that needs to be maintained.',
+  price: 0,
   duration: 3,
   outcome: 'Certificate of Completion',
   valid: 12,
   estimate_data: 300,
   required: [],
   locked: false,
+  lessons: [
+    {
+      id: 'welcome',
+      type: 'video',
+      video: 'welcome.mp4',
+      indexName: 'Welcome',
+      indexDescription: 'Introduction to 托me’s Basic and Assistant Nurse training.',
+      pageName: 'Welcome',
+      pageDescription: `
+      An introduction to Tuome Training.<br>
+      In this video you will learn:<br>
+      - How to train using Tuome.<br>
+      - The progressive steps involved<br>
+      `,
+      videoLength: 45,
+      minimumStay: 45,
+    },
+    {
+      id: 'hand-hygiene',
+      type: 'video',
+      video: 'hand-hygiene.mp4',
+      indexName: 'Hand Hygiene',
+      indexDescription: '',
+      pageName: 'Hand Hygiene',
+      pageDescription: `
+      The transfer of bacteria/germs is most often not though air but though direct skin contact.
+      Ensuring you maintain good hand hygiene reduces the risk you will contract something or carry it to another.
+      Learn the correct technique to ensure your hands are clear and free from germs and the times you should pay attention to clean your hands.
+      `,
+      videoLength: 69,
+      minimumStay: 69,
+    },
+    {
+      id: 'hand-hygiene-quiz',
+      type: 'quiz',
+      indexName: 'Quiz',
+      indexDescription: '',
+      pageName: 'Hand Hygiene Quiz',
+      pageDescription: `
+      Please select the best word for the sentence:
+      `,
+      questions: [
+        {
+          question: 'There are $$$ times you should clean your hands.',
+          options: [
+            'Many',
+            'One',
+            'Two',
+            'Three',
+            'Five',
+            'Scheduled',
+          ],
+          wrongText: 'Incorrect. There are five times that you should clean your hands. Please watch video again.',
+          correct: 'Five',
+        },
+      ],
+    },
+  ],
 };
--- addons/education/1.0.0/courses/level-1/index.js
+++ addons/education/1.0.0/courses/level-1/index.js
@@ -1,8 +1,15 @@
 export default {
   id: 'level-1',
   icon: 'level-1.svg',
-  name: 'Assistant Nurse 托me Level 1',
-  description: `Working with Elders requires additional skill
+  indexName: 'Assistant Nurse 托me Level 1',
+  indexDescription: `Working with Elders requires additional skill
+  and consideration. Assistant Nurse’s are
+  more than just housekeepers that provide
+  basic care to Elders. They are well trained in how to best serve with the interest of Elders at heart.<br>
+  This certificate is valid for 12 months.<br>
+  You may earn a credit to complete this course free of charge if you work for 托me and maintain positive feedback.`,
+  pageName: '托me Level 1',
+  pageDescription: `Working with Elders requires additional skill
   and consideration. Assistant Nurse’s are
   more than just housekeepers that provide
   basic care to Elders. They are well trained in how to best serve with the interest of Elders at heart.<br>
@@ -15,4 +22,5 @@ export default {
   estimate_data: 500,
   required: ['basics'],
   locked: true,
+  lessons: [],
 };
