f6bd5720848ccd4ce5af59a6be08f997bc9f2646
Vladimir Kharlampidi <nolimits4web@gmail.com>
2019-08-28 12:57:55 +0300

    WHS: Initial setup

---
+++ addons/whs/1.0.0/src/pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <f7-page id="invoicing__index" name="invoicing__index" class="invoicing-page">
+    <f7-navbar>
+      <tommy-nav-menu></tommy-nav-menu>
+      <f7-nav-title>Inventory</f7-nav-title>
+    </f7-navbar>
+
+  </f7-page>
+</template>
+<script>
+import API from "../api";
+
+export default {
+  data() {
+    return {
+
+    };
+  },
+  created() {
+
+  },
+  computed: {
+
+  },
+  methods: {
+
+  },
+  beforeDestroy() {
+    const self = this;
+  },
+  mounted() {
+    const self = this;
+  }
+};
+</script>
