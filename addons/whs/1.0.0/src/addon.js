cb64875344be2665cbd9287ab96e32a3c0eacac8
Alexey Z <47769319+Overman775@users.noreply.github.com>
2019-11-15 03:12:22 +0500

    Format js files

--- addons/whs/1.0.0/src/addon.js
+++ addons/whs/1.0.0/src/addon.js
@@ -1,76 +1,76 @@
-import Index from './pages/index.vue';
-import ItemDetails from './pages/item-details.vue';
-import LocationDetails from './pages/location-details.vue';
-import TagDetails from './pages/tag-details.vue';
-import ItemAdd from './pages/item-add.vue';
-import TagAdd from './pages/tag-add.vue';
-import LocationAdd from './pages/location-add.vue';
-import Settings from './pages/settings.vue';
-import SettingsItems from './pages/settings-items.vue';
-import SettingsItemsFields from './pages/settings-items-fields.vue';
-import SettingsItemsFieldDetails from './pages/settings-items-field-details.vue';
-import SettingsLocations from './pages/settings-locations.vue';
-import SettingsTags from './pages/settings-tags.vue';
-import SettingsActivities from './pages/settings-activities.vue';
+import Index from "./pages/index.vue";
+import ItemDetails from "./pages/item-details.vue";
+import LocationDetails from "./pages/location-details.vue";
+import TagDetails from "./pages/tag-details.vue";
+import ItemAdd from "./pages/item-add.vue";
+import TagAdd from "./pages/tag-add.vue";
+import LocationAdd from "./pages/location-add.vue";
+import Settings from "./pages/settings.vue";
+import SettingsItems from "./pages/settings-items.vue";
+import SettingsItemsFields from "./pages/settings-items-fields.vue";
+import SettingsItemsFieldDetails from "./pages/settings-items-field-details.vue";
+import SettingsLocations from "./pages/settings-locations.vue";
+import SettingsTags from "./pages/settings-tags.vue";
+import SettingsActivities from "./pages/settings-activities.vue";
 
 const routes = [
   {
-    path: '/whs/',
+    path: "/whs/",
     component: Index,
-    keepAlive: true,
+    keepAlive: true
   },
   {
-    path: '/whs/item/',
-    component: ItemDetails,
+    path: "/whs/item/",
+    component: ItemDetails
   },
   {
-    path: '/whs/location/',
-    component: LocationDetails,
+    path: "/whs/location/",
+    component: LocationDetails
   },
   {
-    path: '/whs/tag/',
-    component: TagDetails,
+    path: "/whs/tag/",
+    component: TagDetails
   },
   {
-    path: '/whs/item-add/',
-    component: ItemAdd,
+    path: "/whs/item-add/",
+    component: ItemAdd
   },
   {
-    path: '/whs/tag-add/',
-    component: TagAdd,
+    path: "/whs/tag-add/",
+    component: TagAdd
   },
   {
-    path: '/whs/location-add/',
-    component: LocationAdd,
+    path: "/whs/location-add/",
+    component: LocationAdd
   },
   {
-    path: '/whs/settings/',
-    component: Settings,
+    path: "/whs/settings/",
+    component: Settings
   },
   {
-    path: '/whs/settings/items/',
-    component: SettingsItems,
+    path: "/whs/settings/items/",
+    component: SettingsItems
   },
   {
-    path: '/whs/settings/items/fields/',
-    component: SettingsItemsFields,
+    path: "/whs/settings/items/fields/",
+    component: SettingsItemsFields
   },
   {
-    path: '/whs/settings/items/field/',
-    component: SettingsItemsFieldDetails,
+    path: "/whs/settings/items/field/",
+    component: SettingsItemsFieldDetails
   },
   {
-    path: '/whs/settings/locations/',
-    component: SettingsLocations,
+    path: "/whs/settings/locations/",
+    component: SettingsLocations
   },
   {
-    path: '/whs/settings/tags/',
-    component: SettingsTags,
+    path: "/whs/settings/tags/",
+    component: SettingsTags
   },
   {
-    path: '/whs/settings/activities/',
-    component: SettingsActivities,
-  },
+    path: "/whs/settings/activities/",
+    component: SettingsActivities
+  }
 ];
 
 export default routes;
--- addons/whs/1.0.0/src/utils/human-time.js
+++ addons/whs/1.0.0/src/utils/human-time.js
@@ -3,21 +3,27 @@ function humanTime(date) {
   const localTime = moment.utc(date).toDate();
   if (moment(localTime).isValid()) {
     const now = moment();
-    const diff = now.diff(moment(localTime), 'days');
-    if (diff === 1) { return `Yesterday ${moment(localTime).format('h:mm A')}`; }
+    const diff = now.diff(moment(localTime), "days");
+    if (diff === 1) {
+      return `Yesterday ${moment(localTime).format("h:mm A")}`;
+    }
     if (diff === 0) {
-      return `Today ${moment(localTime).format('h:mm A')}`;
+      return `Today ${moment(localTime).format("h:mm A")}`;
     }
     if (diff === -1) {
-      return `Yesterday ${moment(localTime).format('h:mm A')}`;
+      return `Yesterday ${moment(localTime).format("h:mm A")}`;
     }
-    if (diff >= 1 && diff <= 8 || diff >= -8 && diff <= -1) {
-      return moment(localTime).format('ddd h:mm A');
+    if ((diff >= 1 && diff <= 8) || (diff >= -8 && diff <= -1)) {
+      return moment(localTime).format("ddd h:mm A");
     }
-    if (diff >= 365 || diff <= -365) { return moment(localTime).format('MMM D, YYYY h:mm A'); }
-    if (diff >= 8 || diff <= -8) { return moment(localTime).format('MMM D h:mm A'); }
+    if (diff >= 365 || diff <= -365) {
+      return moment(localTime).format("MMM D, YYYY h:mm A");
+    }
+    if (diff >= 8 || diff <= -8) {
+      return moment(localTime).format("MMM D h:mm A");
+    }
   }
-  return 'None';
+  return "None";
 }
 
 export default humanTime;
