b3b16cd72607770013821ceace2b770a67bcac25
Alexey Z <47769319+Overman775@users.noreply.github.com>
2019-11-20 23:26:40 +0500

    Assets multiple picker

--- addons/whs/1.0.0/src/components/assets-picker.vue
+++ addons/whs/1.0.0/src/components/assets-picker.vue
@@ -1,6 +1,6 @@
 <template>
   <li>
-    <a class="item-content no-chevron item-link whs-list-search-item" @click="openSelector">
+    <a class="item-content no-chevron item-link whs-list-search-item" @click="openSelector" v-if="selected.length == 0 || multiply">
       <div class="item-media">
         <i class="icon whs-list-search-icon"></i>
       </div>
@@ -51,6 +51,7 @@ import ListStyles from "../mixins/list-styles.vue";
             multiply: self.multiply,
             getData: self.getData,
             type: "asset",
+            multiply: self.multiply,
             onChange(asset, selected) {                            
               if (selected) {
                 self.$emit('itemAdd', asset);
@@ -75,7 +76,12 @@ import ListStyles from "../mixins/list-styles.vue";
       },
       addItem(target){
         self = this;
-        self.selected.push(target)
+        if(self.multiply){          
+          self.selected.push(target)
+        }else{
+          self.selected.splice(0, 99);
+          self.selected.push(target)
+        }
       },
       deleteItem(target){
         self = this;
