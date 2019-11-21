9084509a8b4834b06c1250f2d1e5d191f8286a6a
Alexey Z <47769319+Overman775@users.noreply.github.com>
2019-11-22 04:14:15 +0500

    Location table

---
+++ addons/whs/1.0.0/src/components/location-table.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="whs-table-container">
+    <template v-if="loaded">
+      <div class="whs-table" v-if="data.length > 0">
+        <table>
+          <thead>
+            <tr>
+              <th class="sort-cell">
+                <a class="link">
+                  <i class="whs-icon whs-icon-sort-black"></i>
+                </a>
+              </th>
+              <th>Location</th>
+              <th>Parent</th>
+              <th>Items</th>
+              <th>Lock</th>
+              <th>Lot code</th>
+              <th>Expiry</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(item, index) in data" :key="'item_'+index">
+              <td class="media-cell"></td>
+              <td>{{item.name}}</td>
+              <td>{{item.parent_location_id}}</td>
+              <td></td>
+              <td v-if="item.active"></td>
+              <td v-else>
+                <i class="whs-summary-icon whs-icon-lock"></i>
+              </td>
+              <td>{{item.sku}}</td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <empty-block v-if="data.length === 0" :text="$t('whs.common.no', {text: settings.location.plural_name})" />
+    </template>
+    <template v-if="!loaded">
+      <div style="background: #fff;">
+        <div class="whs-table skeleton-text skeleton-effect-blink">
+          <table>
+            <thead>
+              <tr>
+                <th class="sort-cell">
+                  <a class="link">
+                    <i class="whs-icon whs-icon-sort-black"></i>
+                  </a>
+                </th>
+                <th>________</th>
+                <th>______</th>
+                <th>____</th>
+                <th>____</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(index) in 12" :key="'skeleton_item_'+index">
+                <td class="media-cell">_</td>
+                <td>___________</td>
+                <td>_____</td>
+                <td>____</td>
+                <td>____</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+import API from "../api";
+import EmptyBlock from "../components/empty-block.vue";
+
+export default {
+  components:{
+    EmptyBlock
+  },
+  props: {
+    loadId: Number,
+    loadIdName: String,
+    parent: String
+  },
+  data() {
+    return {
+      data: [],
+      settings: API.main_page.$data.settings,
+      page: 1,
+      loaded: false
+    };
+  },
+  methods: {
+    loadData(page = 1) {
+      const self = this;
+      const options = {};
+      options[self.loadIdName] = self.loadId;
+      API.getLocations(options).then(data => {
+        self.data = data;
+        self.loaded = true;
+      });
+
+      ///test pagination
+      self.$events.$emit("pagination:" + self.parent + ":location:set", {
+        total: 12,
+        rows: 39
+      });
+    },
+    changePagination(page) {
+      const self = this;
+      self.loaded = false;
+      self.loadData(page);
+    }
+  },
+  created() {
+    const self = this;
+    self.$events.$on(
+      "pagination:" + self.parent + ":location:change",
+      self.changePagination
+    );
+  },
+  beforeDestroy() {
+    const self = this;
+    self.$events.$off(
+      "pagination:" + self.parent + ":location:change",
+      self.changePagination
+    );
+  },
+  mounted() {
+    self = this;
+    self.loadData();
+  }
+};
+</script>
