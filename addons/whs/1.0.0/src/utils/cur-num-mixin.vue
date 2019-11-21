903a08f6a1cd597da9deaa40b55b5e6d06f31a9f
Alexey Z <47769319+Overman775@users.noreply.github.com>
2019-11-22 04:14:58 +0500

    Currency formater fix

--- addons/whs/1.0.0/src/utils/cur-num-mixin.vue
+++ addons/whs/1.0.0/src/utils/cur-num-mixin.vue
@@ -6,8 +6,6 @@ import number from "../utils/filter/number";
 
 export default {
   methods: {
-    $_currency: currency,
-    $_number: number,
     formatCurrency(
       value,
       symbol_code = API.main_page.$data.settings.main.currency,
@@ -16,11 +14,11 @@ export default {
     ) {
       self = this;
       const symbol = self.currencyList[symbol_code];
-      return self.$_currency(value, symbol, dec, options);
+      return currency(value, symbol, dec, options);
     },
     formatNumber(value, format = '0,0', options = {}) {
         if(value === undefined) return '';
-      return self.$_number(value, format, options);
+      return number(value, format, options);
     }
   },
   computed: {
