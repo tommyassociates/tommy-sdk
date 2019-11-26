5769bd6cae0ed0b44a2bede5d353fa3d971e0575
Alexey Z <47769319+Overman775@users.noreply.github.com>
2019-11-27 01:20:25 +0500

    Add gulp-inline-image for gulp, format build js

--- build/build-addon.js
+++ build/build-addon.js
@@ -1,80 +1,93 @@
 /* eslint no-console: off */
-const gulp = require('gulp');
-const rollup = require('rollup');
-const buble = require('rollup-plugin-buble');
-const rollupResolve = require('rollup-plugin-node-resolve');
-const commonjs = require('rollup-plugin-commonjs');
-const vue = require('rollup-plugin-vue');
-const uglify = require('gulp-uglify');
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-const cleanCSS = require('gulp-clean-css');
+const gulp = require("gulp");
+const rollup = require("rollup");
+const buble = require("rollup-plugin-buble");
+const rollupResolve = require("rollup-plugin-node-resolve");
+const commonjs = require("rollup-plugin-commonjs");
+const vue = require("rollup-plugin-vue");
+const uglify = require("gulp-uglify");
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const cleanCSS = require("gulp-clean-css");
+const inlineImage = require("gulp-inline-image");
 
 function buildAddonJs(addonFolder) {
   const path = `./addons/${addonFolder}/1.0.0`;
   return new Promise((resolve, reject) => {
-    rollup.rollup({
-      input: `${path}/src/addon.js`,
-      plugins: [
-        vue(),
-        //rollupResolve({ jsnext: true, browser: true }),
-        rollupResolve({ 
-          mainFields: ['module', 'main', 'jsnext', 'browser']
-        }),
-        commonjs(),
-        buble({
-          objectAssign: 'Object.assign',
-        }),
-      ],
-    }).then((bundle) => { // eslint-disable-line
-      // cache = bundle;
-      return bundle.write({
-        format: 'iife',
-        name: 'addon',
-        strict: false,
-        sourcemap: false,
-        file: `${path}/build/addon.js`,
-      });
-    }).then(() => {
-      // Minified version
-      gulp.src(`${path}/build/addon.js`)
-        .pipe(uglify({ keep_fnames: true }))
-        .pipe(gulp.dest(`${path}/build/`))
-        .on('end', () => {
-          resolve();
+    rollup
+      .rollup({
+        input: `${path}/src/addon.js`,
+        plugins: [
+          vue(),
+          rollupResolve({
+            mainFields: ["module", "main", "jsnext", "browser"]
+          }),
+          commonjs(),
+          buble({
+            objectAssign: "Object.assign"
+          })
+        ]
+      })
+      .then(bundle => {
+        // eslint-disable-line
+        // cache = bundle;
+        return bundle.write({
+          format: "iife",
+          name: "addon",
+          strict: false,
+          sourcemap: false,
+          file: `${path}/build/addon.js`
         });
-    }).catch((err) => {
-      console.log(err);
-      reject(err);
-    });
+      })
+      .then(() => {
+        // Minified version
+        gulp
+          .src(`${path}/build/addon.js`)
+          .pipe(uglify({ keep_fnames: true }))
+          .pipe(gulp.dest(`${path}/build/`))
+          .on("end", () => {
+            resolve();
+          });
+      })
+      .catch(err => {
+        console.log(err);
+        reject(err);
+      });
   });
 }
 function buildAddonScss(addonFolder) {
   const path = `./addons/${addonFolder}/1.0.0`;
   return new Promise((resolve, reject) => {
-    gulp.src(`${path}/src/addon.scss`)
+    gulp
+      .src(`${path}/src/addon.scss`)
       .pipe(sass())
       // .pipe(importCss())
-      .on('error', (err) => {
+      .on("error", err => {
         console.log(err.toString());
         reject(err);
       })
-      .pipe(autoprefixer({
-        cascade: false,
-      }))
-      .on('error', (err) => {
+      .pipe(inlineImage())
+      .pipe(
+        autoprefixer({
+          cascade: false
+        })
+      )
+      .on("error", err => {
         console.log(err.toString());
         reject(err);
       })
       .pipe(gulp.dest(`${path}/build`))
-      .on('end', () => {
-        gulp.src(`${path}/build/addon.css`)
-          .pipe(cleanCSS({
-            advanced: false,
-            aggressiveMerging: false,
-          }))
+      .on("end", () => {
+        gulp
+          .src(`${path}/build/addon.css`)
+          .pipe(
+            cleanCSS({
+              advanced: false,
+              aggressiveMerging: false
+            })
+          )
           .pipe(gulp.dest(`${path}/build/`))
-          .on('end', () => {
+          .on("end", () => {
             resolve();
           });
       });
@@ -83,16 +96,15 @@ function buildAddonScss(addonFolder) {
 function buildAddon(addonFolder) {
   console.log(`Building addon "${addonFolder}"`);
   return new Promise((resolve, reject) => {
-    Promise.all([
-      buildAddonJs(addonFolder),
-      buildAddonScss(addonFolder),
-    ]).then(() => {
-      console.log(`Done addon "${addonFolder}"`);
-      resolve();
-    }).catch((err) => {
-      console.log(`Done addon "${addonFolder}"`);
-      reject(err);
-    });
+    Promise.all([buildAddonJs(addonFolder), buildAddonScss(addonFolder)])
+      .then(() => {
+        console.log(`Done addon "${addonFolder}"`);
+        resolve();
+      })
+      .catch(err => {
+        console.log(`Error addon "${addonFolder}"`, err);
+        reject(err);
+      });
   });
 }
 
