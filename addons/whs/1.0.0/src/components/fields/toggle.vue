b639ea2e3e0128f751edefa3057a9a1620e5ef5d
Alexey Z <47769319+Overman775@users.noreply.github.com>
2019-12-10 04:42:47 +0500

    Toggle field

---
+++ addons/whs/1.0.0/src/components/fields/toggle.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+          <f7-list-item divider>
+            <i class="whs-form-icon whs-form-icon-toggle"></i>
+            {{params.name}}
+          </f7-list-item>
+          <f7-list-item>
+            <f7-toggle
+              slot="after"
+              :checked="value"
+              @toggle:change="updateValue"
+            />
+          </f7-list-item>
+  </div>
+</template>
+
+<script>
+import FieldsLogic from "../../mixins/fileds-logic.vue";
+
+export default {
+  name: "ToggleField",
+  mixins: [FieldsLogic]
+};
+</script>
