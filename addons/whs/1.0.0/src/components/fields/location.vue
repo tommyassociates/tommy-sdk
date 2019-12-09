f6d137804ac8cf41c04a8b7cf1cf31326a0b2484
Alexey Z <47769319+Overman775@users.noreply.github.com>
2019-12-10 02:50:38 +0500

    Location dynamic field

---
+++ addons/whs/1.0.0/src/components/fields/location.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+          <f7-list-item divider>
+            <i class="whs-form-icon whs-form-icon-location"></i>
+            {{params.name}}
+          </f7-list-item>
+          <location-picker
+            :multiply="params.multiply"
+            @selected:change="updateValue"
+            :loadSelect="[value]"
+          />
+  </div>
+</template>
+
+<script>
+import FieldsLogic from "../../mixins/fileds-logic.vue";
+import LocationPicker from "../../components/picker/location.vue";
+
+
+export default {
+  name: "LocationField",
+  components:{
+    LocationPicker
+  },
+  mixins: [FieldsLogic]
+};
+</script>
