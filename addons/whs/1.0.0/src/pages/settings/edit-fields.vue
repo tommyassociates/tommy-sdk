3663d34b2524bd098428247a2fb1ef4089221b7b
Alexey Z <47769319+Overman775@users.noreply.github.com>
2019-12-16 20:27:03 +0500

    Add edit fields form loagic

--- addons/whs/1.0.0/src/pages/settings/edit-fields.vue
+++ addons/whs/1.0.0/src/pages/settings/edit-fields.vue
@@ -10,70 +10,62 @@
       </f7-nav-right>
     </f7-navbar>
 
+    <f7-block-title class="whs-list-divider">{{$t('whs.settings_fields.active')}}</f7-block-title>
     <f7-list
       class="no-margin no-hairlines whs-settings-list whs-fields-list"
-      sortable
-      sortableEnabled
+      sortable      
+      sortable-enabled
+      @sortable:sort="onSort"
+      v-if="activeFields.length > 0"
+      ref='sort_list'
     >
       <ul>
-        <f7-list-item divider :title="$t('whs.settings_fields.active')" />
-        <f7-list-item
-          :title="$t('whs.settings_fields.name_label')"
-          link="/whs/settings/items/field/"
+        <template 
+          v-for="(field, index) in activeFields"
         >
-          <i slot="media" class="whs-form-icon whs-form-icon-aa"></i>
-        </f7-list-item>
-        <f7-list-item
-          :title="$t('whs.settings_fields.description_label')"
-          link="/whs/settings/items/field/"
-        >
-          <i slot="media" class="whs-form-icon whs-form-icon-aa"></i>
-          <div slot="title">{{$t('whs.settings_fields.description_info')}}</div>
-        </f7-list-item>
-        <f7-list-item
-          :title="$t('whs.settings_fields.photo_label')"
-          link="/whs/settings/items/field/"
-        >
-          <i slot="media" class="whs-form-icon whs-form-icon-photo"></i>
-          <div slot="title">{{$t('whs.settings_fields.photo_info')}}</div>
-        </f7-list-item>
-        <f7-list-item
-          :title="$t('whs.settings_fields.value_label')"
-          link="/whs/settings/items/field/"
-        >
-          <i slot="media" class="whs-form-icon whs-form-icon-dollar"></i>
-          <div slot="title">{{$t('whs.settings_fields.value_info')}}</div>
-        </f7-list-item>
-        <f7-list-item
-          :title="$t('whs.settings_fields.weight_label')"
-          link="/whs/settings/items/field/"
-        >
-          <i slot="media" class="whs-form-icon whs-form-icon-int"></i>
-          <div slot="title">{{$t('whs.settings_fields.weight_info')}}</div>
-        </f7-list-item>
+          <f7-list-item
+            :title="field.name"
+            link="/whs/settings/items/field/"
+            :key="'active_field'+index"
+          >
+            <i slot="media" :class="'whs-form-icon whs-form-icon-'+field.type"></i>
+            <div slot="title">{{field.description}}</div>
+          </f7-list-item>
+        </template>
       </ul>
     </f7-list>
-    <f7-list class="no-margin no-hairlines whs-settings-list whs-fields-list">
+    <f7-list class="no-margin no-hairlines whs-settings-list whs-fields-list" v-if="inactiveFields.length > 0">
+      <f7-block-title class="whs-list-divider">{{$t('whs.settings_fields.inactive')}}</f7-block-title>
       <ul>
-        <f7-list-item divider :title="$t('whs.settings_fields.inactive')" />
-        <f7-list-item title="Lot Code" link="/whs/settings/items/field/">
-          <i slot="media" class="whs-form-icon whs-form-icon-aa"></i>
-        </f7-list-item>
+        <template 
+          v-for="(field, index) in inactiveFields"
+        >
+          <f7-list-item
+            :title="field.name"
+            link="/whs/settings/items/field/"
+            :key="'inactive_field'+index"
+          >
+            <i slot="media" :class="'whs-form-icon whs-form-icon-'+field.type"></i>
+            <div slot="title">{{field.description}}</div>
+          </f7-list-item>
+        </template>
       </ul>
     </f7-list>
-    <f7-list class="no-margin no-hairlines whs-settings-list whs-fields-list">
+    <f7-list class="no-margin no-hairlines whs-settings-list whs-fields-list" v-if="inactiveCustomFields.length > 0">
+      <f7-block-title class="whs-list-divider">{{$t('whs.settings_fields.inactive_custom')}}</f7-block-title>
       <ul>
-        <f7-list-item divider :title="$t('whs.settings_fields.inactive_custom')" />
-        <f7-list-item title="Custom Field Name" link="/whs/settings/items/field/">
-          <div slot="title">Custom description</div>
-          <i slot="media" class="whs-form-icon whs-form-icon-calendar"></i>
-        </f7-list-item>
-        <f7-list-item
-          :title="$t('whs.settings_fields.new_custom_field')"
-          link="/whs/settings/items/field/"
+        <template 
+          v-for="(field, index) in inactiveCustomFields"
         >
-          <i slot="media" class="icon f7-icons color-red">add</i>
-        </f7-list-item>
+          <f7-list-item
+            :title="field.name"
+            link="/whs/settings/items/field/"
+            :key="'inactive_custom_field'+index"
+          >
+            <i slot="media" :class="'whs-form-icon whs-form-icon-'+field.type"></i>
+            <div slot="title">{{field.description}}</div>
+          </f7-list-item>
+        </template>
       </ul>
     </f7-list>
   </f7-page>
@@ -82,17 +74,60 @@
 import API from "../../api";
 
 export default {
-  data() {
-    return {};
+  name: "EditFields",
+  created() {
+    const self = this;
+    self.typeEdit = self.$f7route.query.type;
+    const temp_fields = JSON.parse(
+      JSON.stringify(API.main_page.$data.settings[self.typeEdit + "_fields"])
+    );
+    temp_fields.sort((a, b) => (a.order > b.order ? 1 : -1));
+    self.fields = self.fields.concat(temp_fields);
+    self.fields_clone = self.fields_clone.concat(temp_fields);
   },
-  created() {},
-  computed: {},
-  methods: {},
+  computed: {
+    activeFields() {
+      const self = this;
+      fields_active = self.fields_clone.filter(e => e.active === true);
+      return fields_active;
+    },
+    inactiveFields() {
+      const self = this;
+      fields_deactive = self.fields_clone.filter(e => e.active === false && e.custom !== true);
+      return fields_deactive;
+    },
+    inactiveCustomFields() {
+      const self = this;
+      fields_deactive = self.fields_clone.filter(e => e.active === false && e.custom === true);
+      return fields_deactive;
+    }
+  },
+  methods: {
+    onSort(data){
+      const self = this;
+      const from = data.detail.from -1;
+      const to = data.detail.to -1;
+
+      self.fields.splice(to, 0, self.fields.splice(from, 1)[0]);
+      self.fields.forEach((f,index)=>{
+        f.order = index;
+      })      
+    }
+  },
   beforeDestroy() {
     const self = this;
   },
   mounted() {
     const self = this;
+    //self.$refs
+    console.log("TCL: mounted -> self.$refs", self.$refs)
+  },
+  data() {
+    return {
+      typeEdit: null,
+      fields: [],
+      fields_clone: [],
+    };
   }
 };
 </script>
