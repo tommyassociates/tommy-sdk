bd3d3c62d6d39e2803b440173e204a2ba41a11eb
Alexey Z <47769319+Overman775@users.noreply.github.com>
2019-12-19 02:30:13 +0500

    Time Clock: toolbat shadow, toolbar buttons

--- addons/time_clock/1.0.0/src/pages/index.vue
+++ addons/time_clock/1.0.0/src/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <f7-page class="time-clock-main-page">
+  <f7-page class="time-clock-main-page" :page-content="false">
     <f7-navbar>
       <tommy-nav-menu></tommy-nav-menu>
       <f7-nav-title>{{$t('time_clock.index.title')}}</f7-nav-title>
@@ -12,26 +12,34 @@
         </f7-link>
       </f7-nav-right>
     </f7-navbar>
-    <f7-block-title class="time-clock-divider">{{$t('time_clock.index.active_title')}}</f7-block-title>
-    <!--Active -->
-    <div class="time-clock-active" v-if="clock_on">
-      <f7-list media-list class="time-clock-list">
-        <Events 
-          :data="active_data"
-          :active="true"
-        />
-      </f7-list>
-    </div>
-    <div class="time-clock-active" v-if="!clock_on">      
-      <div class="time-clock-avatars-container">
-        <Active-avatar :data="team_data"/>
+    <f7-toolbar :style="toolbarStyle">
+      <f7-button raised fill v-if="!clock_on && !break_on" @click="clockOnClick" class="time-clock-toolbar-button clock-on">{{$t('time_clock.index.clock_on_button')}}</f7-button>
+      <f7-button raised fill v-if="clock_on && !break_on" @click="clockOffClick" class="time-clock-toolbar-button clock-off">{{$t('time_clock.index.clock_off_button')}}</f7-button>
+      <f7-button raised fill v-if="clock_on && !break_on" @click="breakOnClick" class="time-clock-toolbar-button break-on">{{$t('time_clock.index.break_on_button')}}</f7-button>
+      <f7-button raised fill v-if="break_on" @click="breakOffClick" class="time-clock-toolbar-button break-off">{{$t('time_clock.index.break_off_button')}}</f7-button>
+    </f7-toolbar>
+    <f7-page-content :style="pageContentStyle">
+      <f7-block-title class="time-clock-divider">{{$t('time_clock.index.active_title')}}</f7-block-title>
+      <!--Active -->
+      <div class="time-clock-active" v-if="clock_on">
+        <f7-list media-list class="time-clock-list">
+          <Events 
+            :data="active_data"
+            :active="true"
+          />
+        </f7-list>
       </div>
-    </div>
-    <f7-block-title class="time-clock-divider">{{$t('time_clock.index.today_title')}}</f7-block-title>
-    <!--Events -->
-    <Events 
-      :data="events_data"
-    />
+      <div class="time-clock-active" v-if="!clock_on">      
+        <div class="time-clock-avatars-container">
+          <Active-avatar :data="team_data"/>
+        </div>
+      </div>
+      <f7-block-title class="time-clock-divider">{{$t('time_clock.index.today_title')}}</f7-block-title>
+      <!--Events -->
+      <Events 
+        :data="events_data"
+      />
+    </f7-page-content>
   </f7-page>
 </template>
 <script>
@@ -49,7 +57,7 @@ export default {
     const self = this;
     return {
       clock_on: false,
-      start_break: false,
+      break_on: false,
       active_data: [],
       events_data: [],
       team_data: [],
@@ -58,8 +66,50 @@ export default {
   created() {
     const self = this;
   },
-  computed: {},
-  methods: {},
+  computed: {
+    pageContentStyle(){
+      const self = this;
+      if (self.clock_on && !self.break_on){
+        return {
+          paddingBottom: "136px"
+        }
+      }else{
+        return {
+          paddingBottom: "74px"
+        }
+      }
+    },
+    toolbarStyle(){
+      const self = this;
+      if (self.clock_on && !self.break_on){
+        return {
+          height: "136px"
+        }
+      }else{
+        return {
+          height: "74px"
+        }
+      }
+    }
+  },
+  methods: {
+    clockOnClick(){
+      const self = this;
+      self.clock_on = true;
+    },
+    clockOffClick(){
+      const self = this;
+      self.clock_on = false;
+    },
+    breakOnClick(){
+      const self = this;
+      self.break_on = true;
+    },
+    breakOffClick(){
+      const self = this;
+      self.break_on = false;
+    }
+  },
   beforeDestroy() {
     const self = this;
   },
