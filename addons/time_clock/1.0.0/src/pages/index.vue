521a6663525d5c2b918827db8fbb4f99b53d2aac
Alexey Z <47769319+Overman775@users.noreply.github.com>
2019-12-18 23:18:26 +0500

    Time Clock: next interaction(index, api)

--- addons/time_clock/1.0.0/src/pages/index.vue
+++ addons/time_clock/1.0.0/src/pages/index.vue
@@ -4,20 +4,56 @@
       <tommy-nav-menu></tommy-nav-menu>
       <f7-nav-title>{{$t('time_clock.index.title')}}</f7-nav-title>
       <f7-nav-right class="time-clock-navbar-links">
+        <f7-link href="/time-clock/search/" icon-only>
+          <f7-icon f7="search" />
+        </f7-link>
         <f7-link href="/time-clock/settings/" icon-only>
           <f7-icon f7="gear" />
         </f7-link>
       </f7-nav-right>
     </f7-navbar>
+    <f7-block-title class="time-clock-divider">{{$t('time_clock.index.active_title')}}</f7-block-title>
+    <!--Active -->
+    <div class="time-clock-active" v-if="clock_on">
+      <f7-list media-list class="time-clock-list">
+        <Events 
+          :data="active_data"
+          :active="true"
+        />
+      </f7-list>
+    </div>
+    <div class="time-clock-active" v-if="!clock_on">      
+      <div class="time-clock-avatars-container">
+        <Active-avatar :data="team_data"/>
+      </div>
+    </div>
+    <f7-block-title class="time-clock-divider">{{$t('time_clock.index.today_title')}}</f7-block-title>
+    <!--Events -->
+    <Events 
+      :data="events_data"
+    />
   </f7-page>
 </template>
 <script>
 import API from "../api";
+import ActiveAvatar from "../components/circle-avatar.vue";
+import Events from "../components/events.vue";
 
 export default {
+  name: 'TimeClock',
+  components:{
+    ActiveAvatar,
+    Events,
+  },
   data() {
     const self = this;
-    return {};
+    return {
+      clock_on: false,
+      start_break: false,
+      active_data: [],
+      events_data: [],
+      team_data: [],
+    };
   },
   created() {
     const self = this;
@@ -29,6 +65,9 @@ export default {
   },
   mounted() {
     const self = this;
+    API.activeGet().then(data => self.active_data = data);
+    API.teamGet().then(data => self.team_data = data);
+    API.eventsGet().then(data => self.events_data = data);
   }
 };
 </script>
