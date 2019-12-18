357d030a5b90fe0d64af7cce85b394a92b64b7cb
Alexey Z <47769319+Overman775@users.noreply.github.com>
2019-12-18 21:39:36 +0500

    Time Clock: event component

---
+++ addons/time_clock/1.0.0/src/components/events.vue
@@ -0,0 +1,39 @@
+<template>
+  <f7-list media-list :class="[active? 'active' : '', 'time-clock-list']">
+    <f7-list-item
+      v-for="(item, index) in data"
+      :key="'event_'+index"
+      :title="item.name" 
+      link 
+      no-chevron
+    >
+      <div
+        slot="media"
+        class="time-clock-item-image"
+        :style="{backgroundImage: 'url('+item.image+')'}"
+      ></div>
+      <div class="description">{{item.coordinate}}</div>
+      <div slot="content-end" class="time">
+        {{$moment(new Date(item.date)).format('HH:mm')}}
+        <br>
+        {{$t('time_clock.index.clock_event_options')[item.event]}}
+      </div>
+    </f7-list-item>
+  </f7-list>
+</template>
+
+<script>
+export default {
+  nane: "Events",
+  props: {
+    data:{
+      type: Array,
+      default: []
+    },
+    active:{
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
