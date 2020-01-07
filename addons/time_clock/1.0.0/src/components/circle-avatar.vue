7b21ac37bb4c75f52cad46e2b4582a2ffbfa88ab
Alexey Z <47769319+Overman775@users.noreply.github.com>
2020-01-08 02:10:23 +0500

    TIme Clock: update avatars active view

--- addons/time_clock/1.0.0/src/components/circle-avatar.vue
+++ addons/time_clock/1.0.0/src/components/circle-avatar.vue
@@ -1,14 +1,17 @@
 <template>
+<div>
+  <f7-block-title class="time-clock-divider">{{devider}}</f7-block-title>
   <div class="time-clock-circle-avatar">
     <div v-for="(item, index) in data" :key="'avatar_'+index" class="container" v-show="index <=2 || more_clicked" @click="goSearch(item.name)">
-      <div class="image" :style="{ backgroundImage: 'url('+item.image+')'}"></div>
-      <div class="name">{{item.name}}</div>
+      <div class="image" :style="{ backgroundImage: 'url('+item.icon_url+')'}"></div>
+      <div class="name">{{item.user_name}}</div>
     </div>
     <div class="container" @click="clickMore" v-if="!more_clicked && data.length > 3">
       <div class="image more"></div>
       <div class="name more">{{$t('time_clock.index.more_button')}}</div>
     </div>
   </div>
+</div>
 </template>
 
 <script>
@@ -18,7 +21,8 @@ export default {
     data: {
       type: Array,
       default: () => []
-    }
+    },
+    devider: String,
   },
   methods:{
     clickMore(){
