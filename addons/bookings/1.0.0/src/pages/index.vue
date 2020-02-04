910b3e68beb79ec1ee8b73aec8d334fedec70b39
Andriy <and@Office.local>
2020-02-04 23:30:21 +0200

    #15 Init calendar in component

--- addons/bookings/1.0.0/src/pages/index.vue
+++ addons/bookings/1.0.0/src/pages/index.vue
@@ -4,6 +4,11 @@
       <tommy-nav-menu></tommy-nav-menu>
       <f7-nav-title>{{$t('bookings.index.title', 'Bookings')}}</f7-nav-title>
     </f7-navbar>
+
+    <f7-block>
+      <div id="calendar-container"></div>
+    </f7-block>
+
     <f7-list media-list class="no-margin transparent no-chevron" no-hairlines v-if="events && events.length">
       <f7-list-group media-list v-if="currentEvents.length">
         <f7-list-item group-title>Current</f7-list-item>
@@ -25,6 +30,7 @@
         </f7-list-item>
       </f7-list-group>
     </f7-list>
+
     <f7-block v-if="events && !events.length" class="no-data">
       <h2>{{$t('bookings.no_bookings', 'No bookings have been assigned')}}</h2>
       <p>{{$t('bookings.no_bookings_hint', 'Please check again later...')}}</p>
@@ -50,6 +56,22 @@ export default {
       events: fakeEvents,
       today: self.$moment().startOf("day")
     };
+  },
+  mounted() {
+    const self = this
+    const app = self.$app
+    const now = new Date()
+
+    self.calendar = app.f7.calendar.create({
+      containerEl: '#calendar-container',
+      toolbar: false,
+      value: [now],
+      events: self.events,
+      on: {
+        init: calendar => {},
+        change: calendar => {},
+      }
+    })
   },
   computed: {
     currentEvents() {
