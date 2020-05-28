a8b79a7400e7f3b8275fcf148fc6f73cde1e7bd0
gavinbruce <gav@chillburn.com.au>
2020-05-28 18:44:56 +1000

    Update the items string to use singular and plural.

--- addons/time_sheets/1.0.0/src/services/timesheet-service.js
+++ addons/time_sheets/1.0.0/src/services/timesheet-service.js
@@ -24,8 +24,8 @@ const TimesheetService = {
 
       const items = timesheetsItemsData.filter(timesheetsItem => +timesheetsItem.timesheet_id === +timesheet.id);
       const title = TimesheetService.dateRangeFormat(timesheet.start_date, timesheet.end_date, self);
-      //const itemsString = items.length === 1 ? self.$t('time_sheets.index.items_label_singular') : self.$t('time_sheets.index.items_label_plural');
-      const description = `${items.length} Items`;
+      const itemsString = items.length === 1 ? self.$t('time_sheets.index.items_label_singular') : self.$t('time_sheets.index.items_label_plural');
+      const description = `${items.length} ${itemsString}`;
       const workHours = items.reduce((totalHours, item) => +totalHours + +item.work_hours, 0);
       const hours = Math.floor(workHours);
       const minutes = String(parseFloat(workHours - hours).toFixed(2)).replace('0.', '');
