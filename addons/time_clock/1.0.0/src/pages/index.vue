3faa4761175c8d0665ea96e4a5d46f7965d711ef
gavinbruce <gav@chillburn.com.au>
2020-05-28 17:54:11 +1000

    Fix error in time_clock which broke time_sheets.

--- addons/time_clock/1.0.0/src/pages/index.vue
+++ addons/time_clock/1.0.0/src/pages/index.vue
@@ -91,7 +91,7 @@
 </template>
 <script>
   import API from "../api";
-  import AttendandcService from "../services/attendance-service";
+  import AttendanceService from "../services/attendance-service";
 
   import ActiveAvatar from "../components/circle-avatar.vue";
   import Events from "../components/events.vue";
@@ -165,11 +165,11 @@
       },
       formattedAttendanceData() {
         const self = this;
-        return AttendandcService.splitAttendanceIntoDays(self.attendanceData, self);
+        return AttendanceService.splitAttendanceIntoDays(self.attendanceData, self);
       },
       formattedActiveData() {
         const self = this;
-        return AttendandcService.formatAttendanceActive(self.activeData, self);
+        return AttendanceService.formatAttendanceActive(self.activeData, self);
       },
     },
     methods: {
