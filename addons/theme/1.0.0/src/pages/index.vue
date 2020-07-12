e004daacbef2d99ca0b4091f26a18a09f22c4bf6
gavinbruce <gav@chillburn.com.au>
2020-07-12 15:55:22 +1000

    Allow updating of the profile team image in the theme mini program.

--- addons/theme/1.0.0/src/pages/index.vue
+++ addons/theme/1.0.0/src/pages/index.vue
@@ -14,7 +14,27 @@
 
             <f7-row>
               <f7-col width="40" :class="'col--icon'">
-                <circle-avatar :size="74" :url="$root.team.icon_url"></circle-avatar>
+
+
+
+                <div v-if="!$device.cordova">
+                  <div class="profile-image--with-file-input">
+
+                    <div class="profile-header-cover__photo-name-container ">
+                      <circle-avatar :size="74" :url="$root.team.icon_url"></circle-avatar>
+                    </div>
+                    <input type="file" id="photochangefile" ref="photochangefile" @change="onPhotoChangeOnNonCordovaDevice()">
+                  </div>
+                </div>
+
+
+                <a id="change-profile-picture" class="item-link list-button" href="#" @click="changeProfilePicture"
+                   v-if="$device.cordova">
+                  <circle-avatar :size="74" :url="$root.team.icon_url"></circle-avatar>
+                </a>
+
+
+
               </f7-col>
               <f7-col width="60" :class="'col--icon-description'">
                 {{$t('theme.index.profile_icon.summary')}}
@@ -43,6 +63,7 @@
 </template>
 <script>
   import circleAvatar from 'tommy-core/src/components/circle-avatar';
+  import photoChanger from '../utils/photo-changer';
 
   export default {
     components: {
@@ -53,7 +74,43 @@
       };
     },
     methods: {
+      changeProfilePicture() {
+        const self = this;
+        if (!self.allowChangePicture) return;
+        photoChanger.init({
+          url: 'team',
+          f7: self.$f7,
+          success(response) {
+            if (response.icon_url) {
+              if (self.$root.account.icon_url) {
+                self.$root.team.icon_url = response.icon_url;
+              }
+              self.$root.updateAccount();
+            }
+          },
+        });
+        photoChanger.openMenu();
+      },
 
+      /**
+       * This is for use by iPhone. We need to use a hidden <input type="file"> to have access to the camera.
+       */
+      onPhotoChangeOnNonCordovaDevice() {
+        const self = this;
+        photoChanger.init({
+          url: 'team',
+          f7: self.$f7,
+          success(response) {
+            if (response.icon_url) {
+              if (self.$root.account.icon_url) {
+                self.$root.team.icon_url = response.icon_url;
+              }
+              self.$root.updateAccount();
+            }
+          },
+        });
+        photoChanger.upload(self.$refs.photochangefile.files[0]);
+      }
     },
   };
 </script>
