dc6a9cd593349b61e545051fee9b4cb0490f6701
gavinbruce <gav@chillburn.com.au>
2020-09-01 20:28:31 +1000

    Fix issue where camera wasn't working on iPhone. Add time_clock csv export.

--- addons/time_clock/1.0.0/src/mixins/baseToBlob.vue
+++ addons/time_clock/1.0.0/src/mixins/baseToBlob.vue
@@ -1,26 +1,34 @@
-
 <script>
 export default {
   methods: {
     dataURLToBlob(dataURL) {
+      console.log('dataURLToBlob');
+      // console.log(dataURL);
       const BASE64_MARKER = ";base64,";
       if (dataURL.indexOf(BASE64_MARKER) === -1) {
-        const parts = dataURL.split(",");
+        const contentType = 'image/jpeg';
+        const raw = window.atob(dataURL);
+        const rawLength = raw.length;
+        const dataArray = new Uint8Array(rawLength);
+
+        for (let i = 0; i < rawLength; i += 1) {
+          dataArray[i] = raw.charCodeAt(i);
+        }
+
+        return new Blob([dataArray], { type: contentType });
+      } else {
+        const parts = dataURL.split(BASE64_MARKER);
         const contentType = parts[0].split(":")[1];
-        const raw = decodeURIComponent(parts[1]);
-        return new Blob([raw], { type: contentType });
-      }
-      const parts = dataURL.split(BASE64_MARKER);
-      const contentType = parts[0].split(":")[1];
-      const raw = window.atob(parts[1]);
-      const rawLength = raw.length;
-      const dataArray = new Uint8Array(rawLength);
+        const raw = window.atob(parts[1]);
+        const rawLength = raw.length;
+        const dataArray = new Uint8Array(rawLength);
 
-      for (let i = 0; i < rawLength; i += 1) {
-        dataArray[i] = raw.charCodeAt(i);
-      }
+        for (let i = 0; i < rawLength; i += 1) {
+          dataArray[i] = raw.charCodeAt(i);
+        }
 
-      return new Blob([dataArray], { type: contentType });
+        return new Blob([dataArray], {type: contentType});
+      }
     }
   }
 };
--- addons/time_clock/1.0.0/src/pages/search.vue
+++ addons/time_clock/1.0.0/src/pages/search.vue
@@ -3,7 +3,13 @@
            @page:beforeout="pageBeforeOut">
     <f7-navbar>
       <tommy-nav-back force></tommy-nav-back>
-      <f7-nav-title>{{$t('time_clock.search.title')}}</f7-nav-title>
+      <f7-nav-title>{{ $t('time_clock.search.title') }}</f7-nav-title>
+
+      <f7-nav-right class="time-clock-navbar-links">
+        <f7-link href="#" icon-only @click.prevent="csvExport()">
+          <export-icon></export-icon>
+        </f7-link>
+      </f7-nav-right>
     </f7-navbar>
     <f7-page-content ref="pageContent">
       <f7-list class="time-clock-searchbar-list">
@@ -47,150 +53,167 @@
           :loaded="loaded.attendance"
         />
       </template>
-      <div class="not-found" v-if="attendanceData.length === 0"><p>{{$t('time_clock.search.not_found')}}</p></div>
+      <div class="not-found" v-if="attendanceData.length === 0"><p>{{ $t('time_clock.search.not_found') }}</p></div>
 
     </f7-page-content>
   </f7-page>
 </template>
 <script>
-  import API from "../api";
-  import AttendanceService from "../services/attendance-service";
+import API from "../api";
+import AttendanceService from "../services/attendance-service";
 
-  import Events from "../components/events.vue";
-  import dateRangeSelect from 'tommy-core/src/components/date-range-select.vue';
+import Events from "../components/events.vue";
+import dateRangeSelect from 'tommy-core/src/components/date-range-select.vue';
+import exportIcon from '../components/icons/export-icon';
 
-  export default {
-    name: "TimeClockSearch",
-    props: {
-      start_search: {
-        type: String,
-        default: null,
-      }
-    },
-    components: {
-      Events,
-      dateRangeSelect
-    },
-    data() {
+export default {
+  name: "TimeClockSearch",
+  props: {
+    start_search: {
+      type: String,
+      default: null,
+    }
+  },
+  components: {
+    Events,
+    dateRangeSelect,
+    exportIcon,
+  },
+  data() {
+    const self = this;
+    return {
+      dateRange: '',
+      delayTimerSearch: null,
+      search: null,
+      searchEnabled: false,
+      scrollTop: 0,
+
+      loaded: {
+        first: false,
+        active: false,
+        attendance: false,
+      },
+
+      activeData: [],
+      formattedActiveData: {},
+      attendanceData: [],
+      formattedAttendanceData: {},
+    };
+  },
+  created() {
+    const self = this;
+    API.actorId = API.getUserId(self);
+    API.actor = API.getActor(self);
+  },
+  computed: {
+    isSearch() {
       const self = this;
-      return {
-        dateRange: '',
-        delayTimerSearch: null,
-        search: null,
-        searchEnabled: false,
-        scrollTop: 0,
+      return self.dateRange || self.search;
+    }
+  },
+  methods: {
+    pageBeforeIn() {
+      const self = this;
+      self.$refs.pageContent.$el.scrollTop = self.scrollTop;
+    },
+    pageBeforeOut() {
+      const self = this;
+      self.scrollTop = self.$refs.pageContent.$el.scrollTop;
+    },
+    onSearchbarSearch(val) {
+      const self = this;
+      self.search = val;
+      clearTimeout(self.delayTimerSearch);
+      self.delayTimerSearch = setTimeout(() => {
+        self.getSearchData(val)
+      }, 1000);
+    },
+    getSearchData(searchText) {
+      const self = this;
+      self.search = searchText;
+      this.refreshSearchResults();
+    },
+    onSearchbarClear() {
+      const self = this;
+      self.search = '';
+    },
 
-        loaded: {
-          first: false,
-          active: false,
-          attendance: false,
-        },
+    onDateRangeChange(value) {
+      const self = this;
+      console.log('search - date range change: ' + value);
+    },
 
-        activeData: [],
-        formattedActiveData: {},
-        attendanceData: [],
-        formattedAttendanceData: {},
+    onDateRangeSave(value) {
+      const self = this;
+      console.log('search - date range save: ' + value);
+
+      //self.dateRange = value;
+
+      self.refreshSearchResults();
+    },
+
+
+    refreshSearchResults() {
+      const self = this;
+      let otherOptions = {
+        others: self.viewOthers,
       };
-    },
-    created() {
-      const self = this;
-      API.actorId = API.getUserId(self);
-      API.actor = API.getActor(self);
-    },
-    computed: {
-      isSearch() {
-        const self = this;
-        return self.dateRange || self.search;
+      if (self.dateRange) {
+        otherOptions.date_range = self.dateRange;
+        console.log('daterange', self.dateRange);
       }
+      if (self.search) {
+        otherOptions.search = encodeURIComponent(self.search);
+      }
+
+      API.getAttendances({otherOptions}).then(data => {
+        self.attendanceData = AttendanceService.prepareAttendances(data, self);
+        self.formattedAttendanceData = AttendanceService.splitAttendanceIntoDays(self.attendanceData, self);
+        self.loaded.attendance = true;
+        self.loaded.first = true;
+      });
+      API.getAttendancesActive({otherOptions}).then(data => {
+        self.activeData = AttendanceService.prepareAttendance(data, self);
+        self.formattedActiveData = AttendanceService.formatAttendanceActive(self.activeData, self);
+        self.loaded.active = true;
+      });
     },
-    methods: {
-      pageBeforeIn() {
-        const self = this;
-        self.$refs.pageContent.$el.scrollTop = self.scrollTop;
-      },
-      pageBeforeOut() {
-        const self = this;
-        self.scrollTop = self.$refs.pageContent.$el.scrollTop;
-      },
-      onSearchbarSearch(val) {
-        const self = this;
-        self.search = val;
-        clearTimeout(self.delayTimerSearch);
-        self.delayTimerSearch = setTimeout(() => {
-          self.getSearchData(val)
-        }, 1000);
-      },
-      getSearchData(searchText) {
-        const self = this;
-        self.search = searchText;
-        this.refreshSearchResults();
-      },
-      onSearchbarClear() {
-        const self = this;
-        self.search = '';
-      },
 
-      onDateRangeChange(value) {
-        const self = this;
-        console.log('search - date range change: ' + value);
-      },
-
-      onDateRangeSave(value) {
-        const self = this;
-        console.log('search - date range save: ' + value);
-
-        //self.dateRange = value;
-
-        self.refreshSearchResults();
-      },
-
-
-      refreshSearchResults() {
-        const self = this;
-        let otherOptions = {
-          others: self.viewOthers,
-        };
-        if (self.dateRange) {
-          otherOptions.date_range = self.dateRange;
-          console.log('daterange', self.dateRange);
-        }
-        if (self.search) {
-          otherOptions.search = encodeURIComponent(self.search);
-        }
-
-        API.getAttendances({otherOptions}).then(data => {
-          self.attendanceData = AttendanceService.prepareAttendances(data, self);
-          self.formattedAttendanceData = AttendanceService.splitAttendanceIntoDays(self.attendanceData, self);
-          self.loaded.attendance = true;
-          self.loaded.first = true;
-        });
-        API.getAttendancesActive({otherOptions}).then(data => {
-          self.activeData = AttendanceService.prepareAttendance(data, self);
-          self.formattedActiveData = AttendanceService.formatAttendanceActive(self.activeData, self);
-          self.loaded.active = true;
-        });
-      },
-
-
-    },
-    beforeDestroy() {
+    csvExport() {
       const self = this;
-    },
-    mounted() {
-      const self = this;
+      let csvContent = "data:text/csv;charset=utf-8,";
+      const csvColumnHeaders = '"Person","Action","Address","Latitude","Longitude","Timestamp"' + "\n";
 
-      return Promise.all([
-        self.$api.getInstalledAddonPermission(
-          "time_clock",
-          "attendance_other_access",
-          {with_filters: true}
-        )
-      ]).then(v => {
-        self.viewOthers = API.checkPermision(v[0], self);
-        self.refreshSearchResults();
+      csvContent += csvColumnHeaders;
+      self.attendanceData.forEach(item => {
+        csvContent += `"${item.user_name}","${item.status}","${item.address}","${item.latitude}","${item.longitude}","${item.timestamp}"` + "\n";
       });
 
-
+      const data = encodeURI(csvContent);
+      const link = document.createElement("a");
+      link.setAttribute("href", data);
+      link.setAttribute("download", "export.csv");
+      link.click();
     }
-  };
+  },
+  beforeDestroy() {
+    const self = this;
+  },
+  mounted() {
+    const self = this;
+
+    return Promise.all([
+      self.$api.getInstalledAddonPermission(
+        "time_clock",
+        "attendance_other_access",
+        {with_filters: true}
+      )
+    ]).then(v => {
+      self.viewOthers = API.checkPermision(v[0], self);
+      self.refreshSearchResults();
+    });
+
+
+  }
+};
 </script>
