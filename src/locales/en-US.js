00256d8cdbfd6cf58ea6df7e187c85fd11e80faf
Gavin Bruce <gav@chillburn.com.au>
2023-03-31 22:34:29 +1000

    Date and time format translations strings.

--- src/locales/en-US.js
+++ src/locales/en-US.js
@@ -6,5 +6,13 @@ export default {
     ok: 'Ok',
     cancel: 'Cancel',
     save: 'Save'
+  },
+  format: {
+    date: {
+      default: 'YYYY/MM/DD',
+      long: 'MMMM D, YYYY',
+      short: 'MMM D',
+    },
+    time: 'h:mma',
   }
 };
