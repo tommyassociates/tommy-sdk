b139ea55728c43593a1c1ea1596ade0ff4ca504e
Kam Low <auscaster@gmail.com>
2023-06-27 17:27:19 +1000

    .

--- src/components/app.vue
+++ src/components/app.vue
@@ -1,72 +1,62 @@
 <template>
   <f7-app v-bind="params">
-    <!-- <template v-if="coreReady"> -->
-       <!-- -->
-      <f7-panel class="sidebar" left cover v-model:opened="leftPanelOpened">
-        <a
-          class="drag-handle"
-          href="#"
-          @click="toggleLeftPanel"
-        />
-        <f7-view :init="false">
-          <f7-page>
-            <f7-navbar title="Navigation"></f7-navbar>
-            <f7-list>
-              <f7-list-item
-                link="/addons/"
-                title="Addons"
-                reload-all
-                panel-close
-              ></f7-list-item>
-              <f7-list-item
-                link="/views/"
-                title="Views"
-                reload-all
-                panel-close
-              ></f7-list-item>
-              <f7-list-item
-                link="/settings/"
-                title="Settings"
-                reload-all
-                panel-close
-              ></f7-list-item>
-            </f7-list>
-          </f7-page>
-        </f7-view>
-      </f7-panel>
-      <f7-view 
-        main 
-        url="/" 
-        :browser-history="true"
-        browser-history-separator="#"
-        :master-detail-breakpoint="1024"
-      >
+    <f7-panel class="sidebar" left cover v-model:opened="leftPanelOpened">
+      <a
+        class="drag-handle"
+        href="#"
+        @click="toggleLeftPanel"
+      />
+      <f7-view :init="false">
+        <f7-page>
+          <f7-navbar title="Navigation"></f7-navbar>
+          <f7-list>
+            <f7-list-item
+              link="/addons/"
+              title="Addons"
+              reload-all
+              panel-close
+            ></f7-list-item>
+            <f7-list-item
+              link="/views/"
+              title="Views"
+              reload-all
+              panel-close
+            ></f7-list-item>
+            <f7-list-item
+              link="/settings/"
+              title="Settings"
+              reload-all
+              panel-close
+            ></f7-list-item>
+          </f7-list>
+        </f7-page>
       </f7-view>
-        <!-- class="ios-edges"  -->
-    <!-- </template> -->
+    </f7-panel>
+    <f7-view 
+      main 
+      url="/" 
+      :browser-history="true"
+      browser-history-separator="#"
+      :master-detail-breakpoint="1024"
+    >
+    </f7-view>
   </f7-app>
 </template>
 
 <script>
-  // import { events } from 'tommy-core/src/tommy'
-
-  export default {
-    props: {
-      params: Object
+export default {
+  props: {
+    params: Object
+  },
+  created() {
+  },
+  data: () => ({
+    leftPanelOpened: false
+  }),
+  methods: {
+    toggleLeftPanel() {
+      this.leftPanelOpened = !this.leftPanelOpened
     },
-    created() {
-      // events.$once('f7ready', () => {
-      //   this.coreReady = true
-      // })
-    },
-    data: () => ({
-      leftPanelOpened: false,
-      // coreReady: false // use this to toggle to true upon f7ready event
-    }),
-    methods: {
-      toggleLeftPanel() {
-        this.leftPanelOpened = !this.leftPanelOpened
-      },
-    },
-  };
+  },
+};
 </script>
